bc9fd72817d8583972fdfe9a92b1ca541cbe66d1
LimeByte <limebyte@twf.co.nz>
2013-06-13 16:59:18 +1200

    Use proper Java conventions.

--- src/main/java/net/xereo/sbmodule/SbModule.java
+++ src/main/java/net/xereo/sbmodule/SbModule.java
@@ -5,23 +5,20 @@ import com.drdanick.McRKit.module.Module;
 import com.drdanick.McRKit.module.ModuleLoader;
 import com.drdanick.McRKit.module.ModuleMetadata;
 
-public class SbModule extends Module
-{
-    protected SbModule(ModuleMetadata moduleMetadata, ModuleLoader moduleLoader, ClassLoader classLoader,
-                       ToolkitEvent toolkitEvent, ToolkitEvent toolkitEvent2)
-    {
-        super(moduleMetadata, moduleLoader, classLoader, toolkitEvent, toolkitEvent2);
+public class SbModule extends Module {
+
+    protected SbModule(ModuleMetadata meta, ModuleLoader loader, ClassLoader classLoader, ToolkitEvent event1, ToolkitEvent event2) {
+        super(meta, loader, classLoader, event1, event2);
     }
 
     @Override
-    protected void onEnable()
-    {
+    protected void onEnable() {
         // TODO Everything!
     }
 
     @Override
-    protected void onDisable()
-    {
-        // TODO EVerything!
+    protected void onDisable() {
+        // TODO Everything!
     }
+
 }
